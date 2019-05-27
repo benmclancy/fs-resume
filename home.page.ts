ffd4b3cbf65f3058cb5bb9b6741feb9be7d1c4de
benmclancy <50749409+benmclancy@users.noreply.github.com>
2019-05-27 23:50:54 +0100

    Add files via upload

---
+++ home.page.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+export class HomePage {
+
+  public myDateOfBirth: String;
+
+  ionViewDidEnter() {
+    this.myDateOfBirth = "1994-10-03";
+  }
+
+  constructor(public alertController: AlertController) {}
+  async presentAlertMultipleButtons() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['Cancel', 'Open Modal', 'Delete']
+    });
+
+    await alert.present();
+  }
+
+  /*constructor(public toastController: ToastController) {}
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Your settings have been saved.',
+      duration: 2000
+    });
+    toast.present();
+  }*/
+}
